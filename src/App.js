c968290cc891258f0f1d6fd6381e05d133a21f14
Amanda Treutler <amandathedev@gmail.com>
2019-09-01 11:49:21 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import LoginForm from "./components/LoginForm";
 import TwitterMessage from "./components/TwitterMessage";
 
 class App extends Component {
-
   login = ({ username, password }) => {
     console.log(`Logging in ${username} with password ${password}`);
   };
@@ -11,7 +10,6 @@ class App extends Component {
   render() {
     return (
       <div>
-
         <h1>
           <pre>LoginForm</pre>
         </h1>
@@ -20,13 +18,11 @@ class App extends Component {
         <h1>
           <pre>TwitterMessage</pre>
         </h1>
+        {/* Component gets the prop from here */}
         <TwitterMessage maxChars={140} />
-
-
-
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,36 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    // set the initial state
+    this.state = {
+      message: ""
+    };
   }
+
+  handleChange = event => {
+    // log the value of each change
+    console.log(event.target.value);
+    this.setState({
+      message: event.target.value
+    });
+  };
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input
+          type="text"
+          name="message"
+          id="message"
+          value={this.state.message}
+          onChange={this.handleChange}
+        />
+        <p>
+          {this.props.maxChars - this.state.message.length} characters remaining
+        </p>
+        {/* Why doesn't this work? */}
+        <h2>Hello {this.state.messsage} </h2>
       </div>
     );
   }
